736a235370797ed8a83b8005b45d207d69ecea22
Paul Baxter <jareth_2005@yahoo.com>
2025-06-07 14:28:41 -0400

    added tree style instead of just indintation

--- src/ASTNode.cpp
+++ src/ASTNode.cpp
@@ -1,4 +1,4 @@
-// ASTNode.cpp
+﻿// ASTNode.cpp
 #include "ASTNode.h"
 #include <iostream>
 
@@ -6,16 +6,45 @@ std::map<int64_t, std::string> ASTNode::astMap;
 
 void ASTNode::print(int indent) const
 {
-    std::string indentStr(indent, ' ');
-    std::cout << indentStr << astMap[type] << " (value: " << value << ")\n";
-    for (const auto& child : children) {
-        if (std::holds_alternative<std::shared_ptr<ASTNode>>(child)) {
-            std::get<std::shared_ptr<ASTNode>>(child)->print(indent + 4);
+    static thread_local std::vector<bool> lastFlags;
+
+    for (int i = 0; i < indent; ++i) {
+        if (i == indent - 1) {
+            std::cout << (lastFlags[i] ? "+-- " : "|-- ");
         }
         else {
-            const Token& tok = std::get<Token>(child);
-            std::cout << indentStr << "  " << astMap[tok.type]
-                << " ('" << tok.value << "')\n";
+            std::cout << (lastFlags[i] ? "    " : "|   ");
         }
     }
+
+    std::cout << astMap[type] << " (value: " << value << ")\n";
+
+    size_t count = children.size();
+    for (size_t i = 0; i < count; ++i) {
+        bool isLast = (i == count - 1);
+        if (indent >= (int)lastFlags.size())
+            lastFlags.push_back(isLast);
+        else
+            lastFlags[indent] = isLast;
+
+        if (std::holds_alternative<std::shared_ptr<ASTNode>>(children[i])) {
+            std::get<std::shared_ptr<ASTNode>>(children[i])->print(indent + 1);
+        }
+        else {
+            const Token& tok = std::get<Token>(children[i]);
+            for (int j = 0; j <= indent; ++j) {
+                if (j == indent) {
+                    std::cout << (isLast ? "+-- " : "|-- ");
+                }
+                else {
+                    std::cout << (lastFlags[j] ? "    " : "|   ");
+                }
+            }
+            std::cout << astMap[tok.type] << " ('" << tok.value << "')\n";
+        }
+    }
+
+    if (indent < (int)lastFlags.size())
+        lastFlags.resize(indent);
 }
+
